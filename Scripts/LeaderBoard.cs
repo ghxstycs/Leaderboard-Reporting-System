2e9141630d6a1114d4b32a03ddb0945a1cff9f4e
ghxstycs <ghxstyDaDev@outlook.com>
2025-02-20 22:06:30 -0500

    Version 1.1.0

--- Scripts/LeaderBoard.cs
+++ Scripts/LeaderBoard.cs
@@ -10,7 +10,7 @@ using System.Collections.Generic;
 using System.Collections;
 using PlayFab;
 using PlayFab.ClientModels;
-// Removed MonkeyJumpers to help (:
+// Removed MonkeyJumpers to help. (:
 using MonkeyJumpersButtons; // Remove unless you're using my report button.
 
 
